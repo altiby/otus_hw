0bf687c5e2751e47771f74d1414823305aa95dd8
Alexsandr Timoshenko <alexander.timoshenko@skyrocket.by>
2021-09-25 21:32:16 +0300

    HW4 is complited

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,16 +11,58 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	m        sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
-type cacheItem struct {
-	key   string
-	value interface{}
+type cacheData struct {
+	CacheKey   Key
+	CacheValue interface{}
+}
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	l.m.Lock()
+	defer l.m.Unlock()
+	elem, ok := l.items[key]
+	if ok {
+		elem.Value = &cacheData{
+			CacheKey:   key,
+			CacheValue: value,
+		}
+		l.queue.MoveToFront(elem)
+	} else {
+		elem = l.queue.PushFront(&cacheData{
+			CacheKey:   key,
+			CacheValue: value,
+		})
+		l.items[key] = elem
+		if l.queue.Len() > l.capacity {
+			back := l.queue.Back()
+			l.queue.Remove(back)
+			delete(l.items, back.Value.(*cacheData).CacheKey)
+		}
+	}
+	return ok
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	l.m.Lock()
+	defer l.m.Unlock()
+	elem, ok := l.items[key]
+	if ok {
+		l.queue.MoveToFront(elem)
+		return elem.Value.(*cacheData).CacheValue, ok
+	}
+	return nil, ok
+}
+
+func (l *lruCache) Clear() {
+	l.m.Lock()
+	defer l.m.Unlock()
+	l.items = make(map[Key]*ListItem, l.capacity)
+	l.queue = NewList()
 }
 
 func NewCache(capacity int) Cache {
