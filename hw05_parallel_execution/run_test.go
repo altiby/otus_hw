c21f74366a9c6ddef3ac0fb818c7b1d3ec19aab9
Alexsandr Timoshenko <alexander.timoshenko@skyrocket.by>
2021-09-26 11:43:42 +0300

    HW5 is complited

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -1,9 +1,11 @@
 package hw05parallelexecution
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"math/rand"
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -11,6 +13,53 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 )
+
+var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+
+type Task func() error
+
+func worker(ctx context.Context, wg *sync.WaitGroup, tasks <-chan Task, errCounter *int32) {
+	defer wg.Done()
+	for {
+		select {
+		case task, ok := <-tasks:
+			if !ok {
+				return
+			}
+			if err := task(); err != nil {
+				atomic.AddInt32(errCounter, 1)
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+func Run(tasks []Task, n, m int) error {
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	tasksChain := make(chan Task)
+	defer close(tasksChain)
+	wg := &sync.WaitGroup{}
+
+	var errCounter int32 = 0
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go worker(ctx, wg, tasksChain, &errCounter)
+	}
+
+	waitForEnd := func() { cancelFunc(); wg.Wait() }
+
+	for _, task := range tasks {
+		if int(atomic.LoadInt32(&errCounter)) >= m {
+			waitForEnd()
+			return ErrErrorsLimitExceeded
+		}
+		tasksChain <- task
+	}
+	waitForEnd()
+	return nil
+}
 
 func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
