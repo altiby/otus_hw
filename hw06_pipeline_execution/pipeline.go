eb9ab380914b3b9a58d6da178134292fe7a6ca0c
Alexsandr Timoshenko <alexander.timoshenko@skyrocket.by>
2021-09-26 20:20:04 +0300

    hw6 is complited

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,34 @@ type (
 
 type Stage func(in In) (out Out)
 
+func Done(done In, valueStream In) Out {
+	takeStream := make(chan interface{})
+	go func() {
+		defer close(takeStream)
+		for {
+			select {
+			case <-done:
+				return
+			case val, ok := <-valueStream:
+				if !ok {
+					return
+				}
+				takeStream <- val
+			}
+		}
+	}()
+	return takeStream
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	lin := in
+	for _, stage := range stages {
+		select {
+		case <-done:
+			return nil
+		default:
+			lin = Done(done, stage(lin))
+		}
+	}
+	return lin
 }
