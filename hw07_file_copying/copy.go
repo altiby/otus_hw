10075b1a5bef79a66855553d2ac5e24d7ceb73de
Alexsandr Timoshenko <alexander.timoshenko@skyrocket.by>
2021-09-30 18:31:40 +0300

    HW7 in complited

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +13,64 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	inputFile, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("failed create input file %w", err)
+	}
+	defer inputFile.Close()
+
+	if _, err := os.Stat(toPath); err == nil {
+		err = os.Remove(toPath)
+		if err != nil {
+			return fmt.Errorf("failed remove old output file %w", err)
+		}
+	}
+
+	outputFile, err := os.OpenFile(toPath, os.O_CREATE|os.O_RDWR, 0755)
+	if err != nil {
+		return fmt.Errorf("failed create output file %w", err)
+	}
+	defer outputFile.Close()
+
+	stat, err := inputFile.Stat()
+	if err != nil {
+		return fmt.Errorf("failed get input file size %w", err)
+	}
+
+	fileSize := stat.Size()
+	if fileSize == 0 {
+		return ErrUnsupportedFile
+	}
+	if fileSize < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	_, err = inputFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("failed seek input file %w", err)
+	}
+
+	buffer := make([]byte, 256)
+	var writtenByte int64
+	for {
+		readByte, err := inputFile.Read(buffer)
+		if err == nil || err == io.EOF {
+			if limit > 0 && writtenByte+int64(readByte) > limit {
+				readByte = int(limit - writtenByte)
+				err = io.EOF
+			}
+
+			writeByte, writeErr := outputFile.Write(buffer[:readByte])
+			writtenByte += int64(writeByte)
+			if writeErr != nil {
+				return fmt.Errorf("failed write output file %w", writeErr)
+			}
+			if err == io.EOF {
+				break
+			}
+		} else {
+			return fmt.Errorf("failed reade input file %w", err)
+		}
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,78 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func cmpFiles(file1, file2 string) bool {
+	f1, _ := os.Open(file1)
+	defer f1.Close()
+	f2, _ := os.Open(file2)
+	defer f2.Close()
+
+	data1, _ := io.ReadAll(f1)
+	data2, _ := io.ReadAll(f2)
+	if len(data1) != len(data2) {
+		return false
+	}
+
+	for i := 0; i < len(data1); i++ {
+		if data1[i] != data2[i] {
+			return false
+		}
+	}
+
+	return true
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	outfile, err := os.CreateTemp("", "out_test")
+	outfile.Close()
+	defer os.Remove(outfile.Name())
+
+	t.Run("offset 0 limit 0", func(t *testing.T) {
+		require.NoError(t, err)
+		require.NoError(t, Copy("./testdata/input.txt", outfile.Name(), 0, 0))
+		require.FileExists(t, outfile.Name())
+		require.True(t, cmpFiles("./testdata/out_offset0_limit0.txt", outfile.Name()))
+	})
+
+	t.Run("offset 0 limit 10", func(t *testing.T) {
+		require.NoError(t, err)
+		require.NoError(t, Copy("./testdata/input.txt", outfile.Name(), 0, 10))
+		require.FileExists(t, outfile.Name())
+		require.True(t, cmpFiles("./testdata/out_offset0_limit10.txt", outfile.Name()))
+	})
+
+	t.Run("offset 0 limit 1000", func(t *testing.T) {
+		require.NoError(t, err)
+		require.NoError(t, Copy("./testdata/input.txt", outfile.Name(), 0, 1000))
+		require.FileExists(t, outfile.Name())
+		require.True(t, cmpFiles("./testdata/out_offset0_limit1000.txt", outfile.Name()))
+	})
+
+	t.Run("offset 0 limit 10000", func(t *testing.T) {
+		require.NoError(t, err)
+		require.NoError(t, Copy("./testdata/input.txt", outfile.Name(), 0, 10000))
+		require.FileExists(t, outfile.Name())
+		require.True(t, cmpFiles("./testdata/out_offset0_limit10000.txt", outfile.Name()))
+	})
+
+	t.Run("offset 100 limit 1000", func(t *testing.T) {
+		require.NoError(t, err)
+		require.NoError(t, Copy("./testdata/input.txt", outfile.Name(), 100, 1000))
+		require.FileExists(t, outfile.Name())
+		require.True(t, cmpFiles("./testdata/out_offset100_limit1000.txt", outfile.Name()))
+	})
+
+	t.Run("offset 6000 limit 1000", func(t *testing.T) {
+		require.NoError(t, err)
+		require.NoError(t, Copy("./testdata/input.txt", outfile.Name(), 6000, 1000))
+		require.FileExists(t, outfile.Name())
+		require.True(t, cmpFiles("./testdata/out_offset6000_limit1000.txt", outfile.Name()))
+	})
 }
