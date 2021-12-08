8dccb24f0ea398a8f1f2b2554a32a7ab1d90ec1c
Alexsandr Timoshenko <alexander.timoshenko@skyrocket.by>
2021-12-08 09:30:35 +0300

    hw 11 is complited

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,39 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", time.Second*10, "timeout")
+	flag.Parse()
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+
+	client := NewTelnetClient(ctx, net.JoinHostPort(host, port), *timeout, os.Stdin, os.Stdout)
+	if err := client.Connect(); err != nil {
+		_ = fmt.Errorf("failed to connect %w ", err)
+	}
+
+	go func() {
+		client.Send()
+		cancel()
+	}()
+
+	go func() {
+		client.Receive()
+		fmt.Println("Connection was closed by peer")
+		cancel()
+	}()
+
+	<-ctx.Done()
 }
